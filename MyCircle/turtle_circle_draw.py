335bb7326dd05c296ef3f18569de8b0179778f2e
NikyMaggini <80970499+NikyMaggini@users.noreply.github.com>
2022-03-31 11:52:31 +0200

    changed the file turtle_circle_draw.py

--- MyCircle/turtle_circle_draw.py
+++ MyCircle/turtle_circle_draw.py
@@ -1,18 +1,21 @@
 import turtle
 from circle_exercise import MyCircle
 
-list = []
-b = MyCircle(30)
-c = MyCircle(40)
-d = MyCircle(50)
-e = MyCircle(60)
-
-list.append(c)
-list.append(b)
-list.append(b)
-list.append(b)
 
 t = turtle.Turtle()
 
-for i in range(0, len(list) + 1):
-    t.circle(list[i]._radius * i)
+a = MyCircle(15)
+b = MyCircle(100)
+c = MyCircle(200)
+
+
+def draw_circle(x, y, circle):
+    t.pu()
+    t.goto(x, y - circle._radius)
+    t.pd()
+    t.circle(circle._radius)
+
+
+draw_circle(-300, 300, a)
+draw_circle(50, 30, b)
+draw_circle(30, 50, c)
